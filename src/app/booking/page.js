354582ed29b5553af9f67ba5eaf426c688627b91
Austin Jr. Mnthambala <71613111+AustinJrMW@users.noreply.github.com>
2023-08-16 16:17:03 +0200

    Add to Booking Page

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -1,4 +1,5 @@
 import Hero from '@/components/booking/Hero'
+import ImageSlider from '@/components/booking/ImageSlider'
 import { Cinzel } from 'next/font/google';
 
 export const title = Cinzel({
@@ -13,6 +14,7 @@ export default function Booking() {
   return (
     <main className='w-full'>
       <Hero titleFont={title}/>
+      <ImageSlider titleFont={title}/>
     </main>
   )
 }
