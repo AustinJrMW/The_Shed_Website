00abe2194953645e34d2e21a8b9af7fc9d564d51
Austin Jr. Mnthambala <71613111+AustinJrMW@users.noreply.github.com>
2023-08-17 12:29:58 +0200

    Test Icongrid for components

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -1,6 +1,7 @@
 import Hero from '@/components/booking/Hero'
 import ImageSlider from '@/components/booking/ImageSlider'
 import RoomTitle from '@/components/booking/RoomTitle'
+import Icongrid from '@/components/booking/Icongrid'
 import { Cinzel } from 'next/font/google';
 
 export const title = Cinzel({
