d6612d5815d1e3586830dccd35a2ccf2eed3653d
Austin Jr. Mnthambala <71613111+AustinJrMW@users.noreply.github.com>
2023-08-17 23:13:19 +0200

    Update icons with desc

--- src/components/booking/Icongrid.jsx
+++ src/components/booking/Icongrid.jsx
@@ -1,8 +1,9 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
-import { TbAirConditioning  } from "react-icons/tb";
+import { TbAirConditioning, TbMicrowave, TbFridge } from "react-icons/tb";
 import { CgScreen } from "react-icons/cg";
+import { GiTeapotLeaves } from "react-icons/gi";
 
 const Icongrid = ({ titleFont }) => {
   const controls = useAnimation();
@@ -21,16 +22,35 @@ const Icongrid = ({ titleFont }) => {
           </h2>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             <div class="flex min-w-0 gap-x-4">
-            <TbAirConditioning className='h-12 w-12 text-[#E0956D]' size={60}/>
+            <TbAirConditioning className='h-14 w-14 text-[#E0956D]' size={70}/>
               <div class="min-w-0 flex-auto">
                 <p class="text-m font-semibold leading-6 text-white-900">Airconditioning</p>
-                <p class="mt-1 truncate text-sm leading-5 text-white-500">Runs from 12pm to 7am </p>
               </div>
             </div>
-          <CgScreen className='text-[#E0956D]' size={30} /><span>Flatscreen</span>
-          <TbAirConditioning className='text-[#E0956D]' size={30} />
-          <CgScreen className='text-[#E0956D]' size={30} />
-          <TbAirConditioning className='text-[#E0956D]' size={30} />
+            <div class="flex min-w-0 gap-x-4">
+            <CgScreen className='h-14 w-14 text-[#E0956D]' size={70}/>
+              <div class="min-w-0 flex-auto">
+                <p class="text-m font-semibold leading-6 text-white-900">Flatscreen</p>
+              </div>
+            </div>
+            <div class="flex min-w-0 gap-x-4">
+            <GiTeapotLeaves className='h-14 w-14 text-[#E0956D]' size={70}/>
+              <div class="min-w-0 flex-auto">
+                <p class="text-m font-semibold leading-6 text-white-900">Electric Kettle</p>
+              </div>
+            </div>
+            <div class="flex min-w-0 gap-x-4">
+            <TbMicrowave className='h-14 w-14 text-[#E0956D]' size={70}/>
+              <div class="min-w-0 flex-auto">
+                <p class="text-m font-semibold leading-6 text-white-900">Microwave</p>
+              </div>
+            </div>
+            <div class="flex min-w-0 gap-x-4">
+            <TbFridge className='h-14 w-14 text-[#E0956D]' size={70}/>
+              <div class="min-w-0 flex-auto">
+                <p class="text-m font-semibold leading-6 text-white-900">Mini-Fridge</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
