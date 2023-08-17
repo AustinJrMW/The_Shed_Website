92c7850e0f8743ce47c232ccde6e9ec38a89bb98
Austin Jr. Mnthambala <71613111+AustinJrMW@users.noreply.github.com>
2023-08-17 23:16:07 +0200

    Update icon sizes

--- src/components/booking/Icongrid.jsx
+++ src/components/booking/Icongrid.jsx
@@ -22,31 +22,31 @@ const Icongrid = ({ titleFont }) => {
           </h2>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             <div class="flex min-w-0 gap-x-4">
-            <TbAirConditioning className='h-14 w-14 text-[#E0956D]' size={70}/>
+            <TbAirConditioning className='h-12 w-12 text-[#E0956D]' size={60}/>
               <div class="min-w-0 flex-auto">
                 <p class="text-m font-semibold leading-6 text-white-900">Airconditioning</p>
               </div>
             </div>
             <div class="flex min-w-0 gap-x-4">
-            <CgScreen className='h-14 w-14 text-[#E0956D]' size={70}/>
+            <CgScreen className='h-12 w-12 text-[#E0956D]' size={60}/>
               <div class="min-w-0 flex-auto">
                 <p class="text-m font-semibold leading-6 text-white-900">Flatscreen</p>
               </div>
             </div>
             <div class="flex min-w-0 gap-x-4">
-            <GiTeapotLeaves className='h-14 w-14 text-[#E0956D]' size={70}/>
+            <GiTeapotLeaves className='h-12 w-12 text-[#E0956D]' size={60}/>
               <div class="min-w-0 flex-auto">
                 <p class="text-m font-semibold leading-6 text-white-900">Electric Kettle</p>
               </div>
             </div>
             <div class="flex min-w-0 gap-x-4">
-            <TbMicrowave className='h-14 w-14 text-[#E0956D]' size={70}/>
+            <TbMicrowave className='h-12 w-12 text-[#E0956D]' size={60}/>
               <div class="min-w-0 flex-auto">
                 <p class="text-m font-semibold leading-6 text-white-900">Microwave</p>
               </div>
             </div>
             <div class="flex min-w-0 gap-x-4">
-            <TbFridge className='h-14 w-14 text-[#E0956D]' size={70}/>
+            <TbFridge className='h-12 w-12 text-[#E0956D]' size={60}/>
               <div class="min-w-0 flex-auto">
                 <p class="text-m font-semibold leading-6 text-white-900">Mini-Fridge</p>
               </div>
