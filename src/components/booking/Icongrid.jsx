0a875f4b0bcc3c8eaba7ce6ba3e2d0fb15f18127
Austin Jr. Mnthambala <71613111+AustinJrMW@users.noreply.github.com>
2023-08-17 12:10:31 +0200

    Update Booking Page

---
+++ src/components/booking/Icongrid.jsx
@@ -0,0 +1,32 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+
+const Icongrid = ({ titleFont }) => {
+  const controls = useAnimation();
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    controls.start({ opacity: 1, y: 0 });
+  }, [controls]);
+
+  return (
+    <div className="bg-black py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+          Room Amenities
+          </h2>
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <IoTimeOutline className='text-[#E0956D]' size={30} />
+          <IoTimeOutline className='text-[#E0956D]' size={30} />
+          <IoTimeOutline className='text-[#E0956D]' size={30} />
+          <IoTimeOutline className='text-[#E0956D]' size={30} />
+          <IoTimeOutline className='text-[#E0956D]' size={30} />
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Icongrid;
