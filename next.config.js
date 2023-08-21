ff69205bb4b53ad51c8c2d02729f18165545a6ac
Austin Jr. Mnthambala <71613111+AustinJrMW@users.noreply.github.com>
2023-08-21 10:45:00 +0200

    Rectify Update

--- next.config.js
+++ next.config.js
@@ -3,17 +3,6 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com'],
       },
-      
-      output: 'export',
- 
-      // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-      // trailingSlash: true,
-     
-      // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-      // skipTrailingSlashRedirect: true,
-     
-      // Optional: Change the output directory `out` -> `dist`
-      // distDir: 'dist',
 }
 
 module.exports = nextConfig
