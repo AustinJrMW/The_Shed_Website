c686c1854c212e75ece7630637f7d3895be0c773
Austin Jr. Mnthambala <71613111+AustinJrMW@users.noreply.github.com>
2023-08-21 10:29:42 +0200

    Add booking button on booking page

--- src/components/booking/Hero.jsx
+++ src/components/booking/Hero.jsx
@@ -49,6 +49,9 @@ const Hero = ({ titleFont }) => {
           >
             Escape to a world of refined relaxation and stylish seclusion at The Shed Boutique Hotel. Tucked away from the noise of the outside Lilongwe, Malawi, our exclusive retreat offers a sanctuary where you can unwind, rejuvenate, and reconnect with your inner peace.
           </motion.p>
+          <a href='https://b12f7607df90c4e1.sirvoy.me/?rooms=%7B%2213124727%22%3A%5B%5B%5D%5D%7D'>
+          <button className='blur-button'>Open Google Map Link</button>
+          </a>
         </div>
       </div>
 {/*       <div className="md:grid md:grid-cols-3 flex flex-col justify-around md:gap-4 gap-0 mt-20 h-full">
